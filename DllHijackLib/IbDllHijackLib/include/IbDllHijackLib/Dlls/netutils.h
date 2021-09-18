baffa2089b2774bbdc0b958bda95f7086b72fbea
Chaoses Ib <Chaos-es@outlook.com>
2021-09-18 20:39:55 +0800

    Dlls: Add netutils.h

---
+++ DllHijackLib/IbDllHijackLib/include/IbDllHijackLib/Dlls/netutils.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+#ifndef IB_HIJACK_EXPORT
+#include "../Default.h"
+#endif
+
+#define IB_HIJACK_DLL "netutils.dll"
+#define IB_HIJACK_DLL_NOEXT "netutils"
+#define IB_HIJACK_DLL_DIR "C:\\Windows\\System32"
+
+IB_HIJACK_EXPORT(1, NetApiBufferAllocate)
+IB_HIJACK_EXPORT(2, NetApiBufferFree)
+IB_HIJACK_EXPORT(3, NetApiBufferReallocate)
+IB_HIJACK_EXPORT(4, NetApiBufferSize)
+IB_HIJACK_EXPORT(5, NetRemoteComputerSupports)
+IB_HIJACK_EXPORT(6, NetapipBufferAllocate)
+IB_HIJACK_EXPORT(7, NetpIsComputerNameValid)
+IB_HIJACK_EXPORT(8, NetpIsDomainNameValid)
+IB_HIJACK_EXPORT(9, NetpIsGroupNameValid)
+IB_HIJACK_EXPORT(10, NetpIsRemote)
+IB_HIJACK_EXPORT(11, NetpIsRemoteNameValid)
+IB_HIJACK_EXPORT(12, NetpIsShareNameValid)
+IB_HIJACK_EXPORT(13, NetpIsUncComputerNameValid)
+IB_HIJACK_EXPORT(14, NetpIsUserNameValid)
+IB_HIJACK_EXPORT(15, NetpwListCanonicalize)
+IB_HIJACK_EXPORT(16, NetpwListTraverse)
+IB_HIJACK_EXPORT(17, NetpwNameCanonicalize)
+IB_HIJACK_EXPORT(18, NetpwNameCompare)
+IB_HIJACK_EXPORT(19, NetpwNameValidate)
+IB_HIJACK_EXPORT(20, NetpwPathCanonicalize)
+IB_HIJACK_EXPORT(21, NetpwPathCompare)
+IB_HIJACK_EXPORT(22, NetpwPathType)
